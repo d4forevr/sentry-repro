429a9e4d9daeb2dba4f3b4812eb40de3ff6edb73
intexteel <intexteel@gmail.com>
2021-06-22 13:53:32 +0300

    Created repro project for sentry not working.

---
+++ Assets/ErrorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sentry;
+using UnityEngine;
+
+public class ErrorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float _lastTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - _lastTime > 1f)
+        {
+            _lastTime = Time.time;
+            Debug.LogError("Test error");
+            SentrySdk.CaptureMessage("I was right");
+        }
+    }
+}
